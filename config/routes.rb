6a1c96ede15faddf1b24aa7a80e5a60c9e207a3a
Justin Coyne <jcoyne@justincoyne.com>
2020-07-05 16:04:09 -0500

    Set the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :lists
+  resources :lists do
+    resources :items
+  end
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'lists#index'
 end
