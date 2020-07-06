e8ee73d4064b4cf180d02792fc4e60eac6a99d42
Justin Coyne <jcoyne@justincoyne.com>
2020-07-05 22:55:14 -0500

    Add stimulus-reflex actions

--- app/reflexes/item_list_reflex.rb
+++ app/reflexes/item_list_reflex.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
-class ExampleReflex < ApplicationReflex
+class ItemListReflex < ApplicationReflex
+  def toggle
+    # TODO: add session
+    ItemList.find_by(id: element.dataset[:id])&.toggle! :completed
+  end
+
+  def create
+    # TODO: add session
+    item = Item.find_or_initialize_by(name: element[:value])
+    list = List.find(element.dataset[:list_id])
+    if item.save
+      ItemList.create!(item: item, list: list)
+    end
+  end
+
   # Add Reflex methods in this file.
   #
   # All Reflex instances expose the following properties:
